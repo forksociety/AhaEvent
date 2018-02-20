caa2d009e2ec8499e6967df977fd5527203e71b9
Prabhanshu Attri <contact@prabhanshu.com>
2018-02-20 08:48:27 +0530

    added colorful background

--- src/App.js
+++ src/App.js
@@ -7,15 +7,11 @@ const { Content } = Layout;
 class App extends Component {
     render() {
         return (
-                <div className="App">
-                    <Layout className="uk-height-1-1">
-                        <Content className="text-center">
-                            <h1 className="logo">Aha! Event</h1>
-                            <p>Showcasing events, one at a time</p>
-                        </Content>
-                    </Layout>
-                </div>
-                );
+                <Layout>
+                    <span className="logo">Aha! Event</span>
+                    <p className="tagline">Showcasing events, one at a time</p>
+                </Layout>
+               );
     }
 }
 
