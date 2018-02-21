0854f72c8d83c50fd5088f1c0d8c083955cc164e
PrabhanshuAttri <contact@prabhanshu.com>
2018-02-21 19:47:52 +0530

    testing unregister service worker

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import config from 'react-global-configuration';
 import configuration from './config';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 config.set(configuration);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
