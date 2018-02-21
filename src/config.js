bd032d5a9b7bdc40066ab250ce98d53c78a55622
Prabhanshu Attri <contact@prabhanshu.com>
2018-02-21 19:18:04 +0530

    added metadata support

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    siteName: 'Aha! Event',
+    siteUrl: 'https://ahaevent.org',
+    siteAuthor: 'Nirmankarta',
+    siteTagline: 'Showcasing events, one at a time',
+    siteKeywords: 'AhaEvents, Technology, Tech events, open source',
+    siteDescription: 'Showcasing events, one at a time',
+    defaultOgImage: 'img/defaultOgImage.png'
+};
+ 
+export default config;
