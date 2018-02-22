5cc94772b4eddeff6d8396e4b18086fbe5d51f3d
Prabhanshu Attri <contact@prabhanshu.com>
2018-02-23 05:22:29 +0530

    added alt text to image

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends Component {
         return (
                 <Layout>
                     <DocumentMeta {...metaData} />
-                    <p className="text-center"><img className="logo-img" src={ require('./img/logo.png') } /></p>
+                    <p className="text-center">
+                        <img className="logo-img" src={ require('./img/logo.png') } alt="Aha! Event Logo" />
+                    </p>
                     <span className="logo">{config.get('siteName')}</span>
                     <p className="tagline">{config.get('siteTagline')}</p>
                 </Layout>
