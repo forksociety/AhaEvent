a893fa3fde7d7e08b3341804f0add04dc8e4b317
Prabhanshu Attri <contact@prabhanshu.com>
2018-02-24 12:19:31 +0530

    fixing environment check

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import { Layout } from 'antd';
 
 class App extends Component {
     render() {
-        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'prod') {
-            ReactGA.initialize(config.get('gaTrackingId'), {"debug":true,"gaOptions":{"cookieDomain":"none"}});
-        } else {
+        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
             console.log('dev environment');
+        } else {
+            ReactGA.initialize(config.get('gaTrackingId'), {"debug":true,"gaOptions":{"cookieDomain":"none"}});
         }
         let metaData = {}
         return (
