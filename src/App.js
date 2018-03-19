5e14db7bb36979a4f18ae3968c9d371cc6c2ec2a
Prabhanshu Attri <contact@prabhanshu.com>
2018-03-20 05:17:03 +0530

    prettier code

--- src/App.js
+++ src/App.js
@@ -11,51 +11,71 @@ import { Layout, Menu, Icon } from 'antd';
 import eventsData from './events.json';
 
 class App extends Component {
-    render() {
-        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
-            console.log('prod environment');
-            ReactGA.initialize(config.get('gaTrackingId'), {"debug":true,"gaOptions":{"cookieDomain":"none"}});
-        } else {
-            console.log('dev environment');
-        }
-        let metaData = {}
-
-        let menuItems = [];
-        let menuData = config.get('menu');
-        menuData.forEach(function(e) {
-            let menuItemLink = <a href={ e.link }><Icon type={ e.icon } /></a>;
-            if (typeof e.newTab !== 'undefined' && e.newTab) {
-                menuItemLink = <a href={ e.link } target="_blank"><Icon type={ e.icon } /></a>;
-            }
-            menuItems.push(
-                    <Menu.Item
-                        key={ e.key }
-                        style={{ float: 'right', color: '#fff', fontSize: '150%', borderBottom: '0px', padding: '0 10px 0px 0px' }}
-                    >
-                    { menuItemLink }
-                    </Menu.Item>
-                    );
-        });
-
-        return (
-                <Layout style={{ background: "#f0f2f5" }}>
-                    <Layout className="awesome-bg">
-                        <DocumentMeta {...metaData} />
-                        <Menu
-                            selectedKeys={[ 'home' ]}
-                            mode="horizontal"
-                            style={{ borderBottom: '0px', background: 'rgba(255, 255, 255, 0)' }}
-                        >
-                            { menuItems }
-                        </Menu>
-                        <span className="logo">{ config.get('siteName') }</span>
-                        <p className="tagline">{ config.get('siteTagline') }</p>
-                    </Layout>
-
-                    <CustomGrid {...{"items": eventsData } } />
-                </Layout>
-               );
+  render() {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
+      console.log('prod environment');
+      ReactGA.initialize(config.get('gaTrackingId'), {
+        debug: true,
+        gaOptions: { cookieDomain: 'none' }
+      });
+    } else {
+      console.log('dev environment');
     }
+    let metaData = {};
+
+    let menuItems = [];
+    let menuData = config.get('menu');
+    menuData.forEach(function(e) {
+      let menuItemLink = (
+        <a href={e.link}>
+          <Icon type={e.icon} />
+        </a>
+      );
+      if (typeof e.newTab !== 'undefined' && e.newTab) {
+        menuItemLink = (
+          <a href={e.link} target="_blank">
+            <Icon type={e.icon} />
+          </a>
+        );
+      }
+      menuItems.push(
+        <Menu.Item
+          key={e.key}
+          style={{
+            float: 'right',
+            color: '#fff',
+            fontSize: '150%',
+            borderBottom: '0px',
+            padding: '0 10px 0px 0px'
+          }}
+        >
+          {menuItemLink}
+        </Menu.Item>
+      );
+    });
+
+    return (
+      <Layout style={{ background: '#f0f2f5' }}>
+        <Layout className="awesome-bg">
+          <DocumentMeta {...metaData} />
+          <Menu
+            selectedKeys={['home']}
+            mode="horizontal"
+            style={{
+              borderBottom: '0px',
+              background: 'rgba(255, 255, 255, 0)'
+            }}
+          >
+            {menuItems}
+          </Menu>
+          <span className="logo">{config.get('siteName')}</span>
+          <p className="tagline">{config.get('siteTagline')}</p>
+        </Layout>
+
+        <CustomGrid {...{ items: eventsData }} />
+      </Layout>
+    );
+  }
 }
 
 export default App;
