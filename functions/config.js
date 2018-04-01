859c1299d8b9ab72bdc3b195bd50824be9966ac5
PrabhanshuAttri <nirmankarta@gmail.com>
2018-04-02 01:27:05 +0530

    added config and updated packages-lock

---
+++ functions/config.js
@@ -0,0 +1,13 @@
+const config = {
+  allowedOrigins: [
+    'http://localhost:3000',
+    'http://localhost:5000',
+    'https://fb.ahaevent.org',
+    'https://ahaevent.org',
+    'https://dev.ahaevent.org',
+    'https://ahaeventorg.firebaseapp.com',
+    'https://dev-ahaeventorg.firebaseapp.com'
+  ]
+}
+
+module.exports = config
