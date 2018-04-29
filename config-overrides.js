2d92769ce28af69f4649d68d2de81b3ec6941277
PrabhanshuAttri <nirmankarta@gmail.com>
2018-04-29 11:13:29 +0530

    enforcing standardjs

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { injectBabelPlugin } = require('react-app-rewired')
 
-module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-    return config;
-};
+module.exports = function override (config, env) {
+  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
+  return config
+}
