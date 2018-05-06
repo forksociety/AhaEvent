3f0baada0b2f3729545224199b2f0c9d84a34a00
Prabhanshu Attri <contact@prabhanshu.com>
2018-05-06 11:25:06 +0530

    added custom footer

---
+++ src/components/CustomFooter/custom-footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import config from 'react-global-configuration'
+import { Layout } from 'antd'
+
+const { Footer } = Layout
+
+class CustomFooter extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <Footer style={{ textAlign: 'center' }}>
+        <a href={config.get('appAuthorSite')} target='_blank'>
+          { config.get('appAuthor') }
+        </a> | {config.get('license')}
+      </Footer>
+    )
+  }
+}
+
+export default CustomFooter
