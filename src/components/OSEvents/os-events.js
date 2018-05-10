92a092b58673114668713a5238372de3233144c4
PrabhanshuAttri <nirmankarta@gmail.com>
2018-05-10 15:12:36 +0530

    added events filter and sortby UI

--- src/components/OSEvents/os-events.js
+++ src/components/OSEvents/os-events.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import config from 'react-global-configuration'
-import { Collapse, Form, Checkbox } from 'antd'
+import { Collapse, Form, Radio, Checkbox, Icon } from 'antd'
 
+import AppStrings from '../../config/app-strings'
 import CustomGrid from '../CustomGrid/custom-grid'
 import { defaultResponse, generateResponse } from '../DefaultResponse/default-response'
 
@@ -11,61 +12,86 @@ const FormItem = Form.Item
 class OSEvents extends Component {
   constructor (prop) {
     super(prop)
-    let filterItems = [
-      {
-        id: 'pastEvents',
-        text: 'Show Past Events'
-      },
-      {
-        id: 'cfpOpen',
-        text: 'CFP Open'
-      }
-    ]
-    let filterInitial = {}
-    for (let k of filterItems) {
-      filterInitial[k.id] = true
+    let searchQueryItems = config.get('searchQueryItems')
+    let allFilters = searchQueryItems.filters
+    let allSortBy = searchQueryItems.sortBy
+    let filterInitialState = {}
+
+    for (let k in allFilters) {
+      filterInitialState[k] = false
     }
+
     this.state = {
       api: config.get('api'),
       events: [],
-      sortby: 'date-a',
+      sortBy: '',
       filterComponents: [],
-      filterCheck: filterInitial,
-      filterItems: filterItems
+      sortByComponents: [],
+      filterState: filterInitialState,
+      allFilters: allFilters,
+      allSortBy: allSortBy,
+      updateDom: false
     }
+    this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.handleSortByChange = this.handleSortByChange.bind(this)
   }
 
   generateApiUrlWithQuery () {
-    let filterStr = Object.keys(this.state.filterCheck)
-      .filter((key) => this.state.filterCheck[key])
+    let filterStr = Object.keys(this.state.filterState)
+      .filter((key) => this.state.filterState[key])
       .map((key) => { return key })
-    console.log(filterStr, this.state.api.eventsUrl + '?sortby=' + this.state.sortby + '&filter=' + filterStr)
-    return this.state.api.eventsUrl + '?sortby=' + this.state.sortby + '&filter=' + filterStr
+
+    let query = ''
+    if (this.state.sortBy.length > 0 || filterStr.length > 0) {
+      query = '?'
+    }
+    if (this.state.sortBy.length > 0) {
+      query += AppStrings.queryParameters.sortBy + '=' +
+        this.state.sortBy +
+        (filterStr.length > 0 ? '&' : '')
+    }
+    if (filterStr.length > 0) {
+      query += AppStrings.queryParameters.filters + '=' + filterStr
+    }
+
+    let url = this.state.api.eventsUrl + query
+    return url
   }
 
   componentWillMount () {
     let filters = []
-    for (let k of this.state.filterItems) {
-      let id = k.id
-      // console.log('loop', id, k.id)
+    for (let k in this.state.allFilters) {
+      let id = k
       filters.push(
         <Checkbox
           id={id}
           key={id}
-          value={this.state.filterCheck[id]}
-          onChange={this.handleChange}
+          value={this.state.filterState[id]}
+          onChange={this.handleFilterChange}
         >
-          {k.text}
+          {this.state.allFilters[k]}
         </Checkbox>
       )
     }
     this.setState({filterComponents: filters})
+
+    let sortBy = []
+    for (let k in this.state.allSortBy) {
+      sortBy.push(
+        <Radio.Button key={k} value={k}>
+          {this.state.allSortBy[k].text} <Icon type={this.state.allSortBy[k].icon} />
+        </Radio.Button>
+      )
+    }
+    this.setState({sortByComponents: sortBy})
     this._loadContent()
   }
 
-  /* componentWillUpdate() {
-    this._loadContent()
-  } */
+  componentDidUpdate () {
+    if (this.state.events.length === 0) {
+      this._loadContent()
+    }
+  }
 
   _loadContent () {
     fetch(this.generateApiUrlWithQuery())
@@ -84,11 +110,17 @@ class OSEvents extends Component {
       })
   }
 
-  handleChange (e) {
-    let tmpFilter = this.state.filterCheck
+  handleFilterChange (e) {
+    let tmpFilter = this.state.filterState
     tmpFilter[e.target.id] = e.target.checked
-    this.setState({filterCheck: tmpFilter})
-    console.log(e, e.target.id, this.state.filterCheck)
+    this.setState({filterState: tmpFilter})
+    this.setState({updateDom: true})
+    this.setState({events: []})
+  }
+
+  handleSortByChange (e) {
+    this.setState({sortBy: e.target.value})
+    this.setState({events: []})
   }
 
   render () {
@@ -100,6 +132,9 @@ class OSEvents extends Component {
               <FormItem style={{margin: '0px'}}>
                 {this.state.filterComponents}
               </FormItem>
+              <Radio.Group defaultValue={AppStrings.sortBy.DATE_ASC} onChange={this.handleSortByChange}>
+                {this.state.sortByComponents}
+              </Radio.Group>
             </Panel>
           </Collapse>
         </div>
