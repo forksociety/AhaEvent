af9fa450cac12c4b9d0313f20670c831bd3ec4ab
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-05-20 23:32:24 -0400

    Updated scripts

--- scripts/travis/deploy.py
+++ scripts/travis/deploy.py
@@ -61,19 +61,20 @@ def getResponseFromMessage(message):
     return response
 
 def createCertificate():
-    fields = {
-        'type': 'SERVICE_ACCOUNT_TYPE',
-        'project_id': 'SERVICE_ACCOUNT_PROJECT_ID',
-        'private_key_id': 'SERVICE_ACCOUNT_PRIVATE_KEY_ID',
-        'private_key': 'SERVICE_ACCOUNT_PRIVATE_KEY',
-        'client_email': 'SERVICE_ACCOUNT_CLIENT_EMAIL',
-        'client_id': 'SERVICE_ACCOUNT_CLIENT_ID',
-        'auth_uri': 'SERVICE_ACCOUNT_AUTH_URI',
-        'token_uri': 'SERVICE_ACCOUNT_TOKEN_URI',
-        'auth_provider_x509_cert_url': 'SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL',
-        'client_x509_cert_url': 'SERVICE_ACCOUNT_CLIENT_X509_CERT_URL'
-    }
-    serviceAccount = {field: os.getenv(fields[field]) for field in fields.keys()}
+    # fields = {
+    #     'type': 'SERVICE_ACCOUNT_TYPE',
+    #     'project_id': 'SERVICE_ACCOUNT_PROJECT_ID',
+    #     'private_key_id': 'SERVICE_ACCOUNT_PRIVATE_KEY_ID',
+    #     'private_key': 'SERVICE_ACCOUNT_PRIVATE_KEY',
+    #     'client_email': 'SERVICE_ACCOUNT_CLIENT_EMAIL',
+    #     'client_id': 'SERVICE_ACCOUNT_CLIENT_ID',
+    #     'auth_uri': 'SERVICE_ACCOUNT_AUTH_URI',
+    #     'token_uri': 'SERVICE_ACCOUNT_TOKEN_URI',
+    #     'auth_provider_x509_cert_url': 'SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL',
+    #     'client_x509_cert_url': 'SERVICE_ACCOUNT_CLIENT_X509_CERT_URL'
+    # }
+    # serviceAccount = {field: os.getenv(fields[field]) for field in fields.keys()}
+    serviceAccount = json.loads(os.getenv('SERVICE_ACCOUNT'))
     return serviceAccount
 
 def saveCertificate(serviceAccount):
